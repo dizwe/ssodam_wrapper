86d6aa89cdbc9b0cd7e0fa9205ad530f15beeb0a
dizwe <dizwe2716@gmail.com>
2019-07-16 17:02:15 +0900

    자주 쓰던 구조로 바꾸기

---
+++ src/components/MainPage.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class MainPage extends Component{
+    render(){
+        return (
+            <h1>TEST</h1>
+        );
+        
+    }
+}
+
+export default MainPage
