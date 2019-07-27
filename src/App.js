03da7e5d8c03f3c079d381f5b4132cfb216beadc
dizwe <dizwe2716@gmail.com>
2019-07-27 21:55:57 +0900

    TEST 1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React, {Component} from 'react';
 import MainPage from './components/MainPage.js';
 import './App.css';
+import ReactGA from 'react-ga';
+
+function initializeReactGA() {
+  ReactGA.initialize('UA-143912727-1');
+  ReactGA.pageview('/');
+}
 
 class App extends Component {
   render(){
+  initializeReactGA();
+
   return (
     <div className="App">
       <MainPage/>
--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,9 +1,25 @@
 import React, {Component} from 'react';
 
+import TagManager from 'react-gtm-module';
+
 class MainPage extends Component{
+
     render(){
+        TagManager.initialize({
+            gtmId: 'GTM-NF3W98T'
+          });
         return (
-            <h1>TEST</h1>
+            <div>
+                <form action="/action_page.php">
+                First name: <input type="text" name="fname"></input>
+                Last name: <input type="text" name="lname"></input>
+                <input type="submit" value="Submit"></input>
+                <a href ="#hi" id="testing">testing</a>
+                <a href ="#wooo" id="mbc">mbc</a>
+                <a href ="https://www.kbs.co.kr" id="kbs">kbs</a>
+                <a id="sbs">sbs</a>
+                </form>
+            </div>
         );
         
     }
